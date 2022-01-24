8679da9bbeb8da7444a649e488a760648d3266c7
Pwcca92 <95024140+Pwcca92@users.noreply.github.com>
2022-01-24 15:57:07 +0100

    ajout entity
    ajout entity category & movie + migration db

---
+++ src/Entity/Movie.php
@@ -0,0 +1,131 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\MovieRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=MovieRepository::class)
+ */
+class Movie
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $title;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $overview;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $release_date;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $poster_path;
+
+    /**
+     * @ORM\ManyToMany(targetEntity=Category::class, mappedBy="name")
+     */
+    private $categories;
+
+    public function __construct()
+    {
+        $this->categories = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getOverview(): ?string
+    {
+        return $this->overview;
+    }
+
+    public function setOverview(string $overview): self
+    {
+        $this->overview = $overview;
+
+        return $this;
+    }
+
+    public function getReleaseDate(): ?string
+    {
+        return $this->release_date;
+    }
+
+    public function setReleaseDate(string $release_date): self
+    {
+        $this->release_date = $release_date;
+
+        return $this;
+    }
+
+    public function getPosterPath(): ?string
+    {
+        return $this->poster_path;
+    }
+
+    public function setPosterPath(string $poster_path): self
+    {
+        $this->poster_path = $poster_path;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Category[]
+     */
+    public function getCategories(): Collection
+    {
+        return $this->categories;
+    }
+
+    public function addCategory(Category $category): self
+    {
+        if (!$this->categories->contains($category)) {
+            $this->categories[] = $category;
+            $category->addName($this);
+        }
+
+        return $this;
+    }
+
+    public function removeCategory(Category $category): self
+    {
+        if ($this->categories->removeElement($category)) {
+            $category->removeName($this);
+        }
+
+        return $this;
+    }
+}
